245f975dafc6f28f407640c509abacf0b68c540d
Harish Ravi <harishamudha@gmail.com>
2019-08-27 14:53:36 +0530

    Project code update

---
+++ src/page/homepage.component.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './homepage.style.scss'
+
+const HomePage = () => (
+    <div class='homepage'>
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">WOMEN</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">WOMEN</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">WOMEN</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">WOMEN</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default HomePage;
