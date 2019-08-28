6884bb7f7689fe9ca78f6ff5492eb3e376d1abdb
Harish Ravi <harishamudha@gmail.com>
2019-08-28 11:41:09 +0530

    Project code update

--- src/components/product/productCard.component.jsx
+++ src/components/product/productCard.component.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
-const ProductCard = ({title}) => (
-    <div className="menu-item">
-        <div className="content">
+const ProductCard = ({title,imageUrl,size}) => (
+    <div 
+        className={`${size} menu-item`} 
+    >   
+        <div className='background-image grow'
+             style={{
+                backgroundImage: `url(${imageUrl})`
+             }}
+        ></div>
+        <div className="content bg-near-white o-80">
             <h1 className="title">{title}</h1>
             <span className="subtitle">SHOP NOW</span>
         </div>
--- src/components/product/productDirectory.components.jsx
+++ src/components/product/productDirectory.components.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import ProductCard from './productCard.component';
 const ProductDirectory = ({menuItems}) => (
     <div className='directory-menu'>
-        {menuItems.map(x => <ProductCard title={x} />)}
+        { 
+            menuItems.map(({title,id, imageUrl, size}) => (
+                <ProductCard title={title} key={id} imageUrl={imageUrl} size={size} />
+            )) 
+        }
     </div>
 );
 
